19dc859d4c00cd2ff3bb078f15a9f21c5987111d
dinesh <dineshpy07@gmail.com>
2018-10-25 12:30:08 +0530

    Add original manager to posts model

--- posts/models.py
+++ posts/models.py
@@ -2,15 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from db.paranoia import ParanoidModel
 
-class Post(models.Model):
+
+class Post(ParanoidModel):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class Comment(models.Model):
+class Comment(ParanoidModel):
     text = models.TextField(null=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
     created_at = models.DateTimeField(auto_now_add=True)
