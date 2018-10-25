4d60d87d3ab7c2c6694d8a96ee5709cc73904dde
dinesh <dineshpy07@gmail.com>
2018-10-25 11:24:42 +0530

    Create posts app

---
+++ posts/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
